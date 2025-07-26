2cbae80ae7e5cc2cdda60d722457f47595156de4
Dambar Sing <programmer.gshi18@gmail.com>
2025-07-26 11:27:02 +0545

    setup

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+let app = require('express')();
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Dambar!'); 
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
