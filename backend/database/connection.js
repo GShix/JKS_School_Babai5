7ab3a20b2c56d87f6c7486ad1a9d9a2fdcf5145f
Dambar Sing <programmer.gshi18@gmail.com>
2025-07-28 21:16:59 +0545

    sensitive

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,6 +1,5 @@
 const {sequelize, DataTypes} = require('sequelize');
 
-const sequelize = new sequelize('postgresql://postgres.jcjowrzhcfkrmjerwsry:DP_SASS.2025@aws-0-us-east-2.pooler.supabase.com:6543/postgres');
 
 sequelize.authenticate()
   .then(() => {
