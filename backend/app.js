543f8859b68db801fdc5593d2bdeb69899e8424f
Dambar Sing <programmer.gshi18@gmail.com>
2025-07-31 19:25:48 +0545

    addProgram

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,51 @@
-let app = require('express')();
+const db = require('./database/connection');
+const { programs } = db;
 
+const express = require('express');
+const app = express();
 
-require('./database/connection');
+app.use(express.json());
 
+// const cors = require('cors');
+// app.use(cors({
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));  
 
-
-app.get('/', (req, res) => {
-  res.send('Hello Dambar!'); 
+app.get("/programs", async(req, res) => {
+  const allPrograms = await programs.findAll();
+  res.json({
+    message: 'List of programs will be here',
+    data: allPrograms
+  })
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+
+app.post("/programs", async(req, res) => {
+  try {
+    const {programName, programDescription, programStatus, programImage} = req.body;
+    
+    const newProgram = await programs.create({
+      programName,
+      programDescription,
+      programStatus,
+      programImage
+    });
+
+    res.json({
+      message: 'Program created successfully',
+      data: newProgram
+    });
+  } catch (error) {
+    console.error('Error creating program:', error);
+    res.status(500).json({
+      message: 'Error creating program',
+      error: error.message
+    });
+  }
+});
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
 });
