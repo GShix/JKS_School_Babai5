62852b241eda6291e361e1733769c555d15978fc
Dambar Sing <programmer.gshi18@gmail.com>
2025-08-01 08:51:40 +0545

    edit program

--- backend/controllers/programController.js
+++ backend/controllers/programController.js
@@ -46,3 +46,56 @@ exports.fetchSingleProgram = async(req, res) => {
     });
   }
 }
+
+exports.deleteProgram = async(req, res) => {
+  const { id } = req.params;
+  const program = await programs.findByPk(id);
+  if (program) {
+    await programs.destroy({ 
+      where: 
+      { 
+        id 
+      } 
+    });
+    res.json({
+      message: 'Program deleted successfully'
+    });
+  } else {
+    res.status(404).json({
+      message: 'Program not found'
+    });
+  }
+}
+
+exports.updateProgram = async(req, res) => {
+  const { id } = req.params;
+  const { programName, programDescription, programStatus, programImage } = req.body;
+
+  try {
+    const program = await programs.findByPk(id);
+    if (!program) {
+      return res.status(404).json({
+        message: 'Program not found'
+      });
+    }
+
+    const updatedProgram = await program.update({
+      programName,
+      programDescription,
+      programStatus,
+      programImage
+    });
+    // const updatedProgram = await programs.update({},{where:{id:id}});
+
+    res.json({
+      message: 'Program updated successfully',
+      data: updatedProgram
+    });
+  } catch (error) {
+    console.error('Error updating program:', error);
+    res.status(500).json({
+      message: 'Error updating program',
+      error: error.message
+    });
+  }
+}
--- backend/routes/programRoute.js
+++ backend/routes/programRoute.js
@@ -1,8 +1,8 @@
-const { fetchPrograms, addProgram, fetchSingleProgram } = require('../controllers/programController');
+const { fetchPrograms, addProgram, fetchSingleProgram, deleteProgram, updateProgram } = require('../controllers/programController');
 
 const router = require('express').Router();
 
 router.route("/programs").get(fetchPrograms).post(addProgram);
-router.route("/programs/:id").get(fetchSingleProgram);
+router.route("/programs/:id").get(fetchSingleProgram).delete(deleteProgram).patch(updateProgram);
 
 module.exports = router;
